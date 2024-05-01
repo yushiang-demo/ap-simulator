a395b2c084f8107ddd625de98bb3fa3de13e88a8
yushiang <tsengyushiang@gmail.com>
2024-05-01 23:22:47 +0800

    feature: initialize pathFinder.

--- src/ThreeApp.js
+++ src/ThreeApp.js
@@ -1,33 +1,79 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import PathFinder from "./PathFinder";
 
+const MODEL = "/ap-simulator/demo.glb";
+const NAV_MODE = "/ap-simulator/demo.nav.glb";
 function ThreeApp(canvas) {
   const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
   const scene = new THREE.Scene();
+  const pathFinder = new PathFinder(NAV_MODE);
   const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
   );
+
+  const { setCamera, firstPersonViewCamera } = (() => {
+    const firstPersonViewCamera = new THREE.PerspectiveCamera(
+      30,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      3
+    );
+    const helper = new THREE.CameraHelper(firstPersonViewCamera);
+    scene.add(firstPersonViewCamera);
+    scene.add(helper);
+    const setCamera = ([x, y, z], [lookAtX, lookAtY, lookAtZ]) => {
+      firstPersonViewCamera.position.set(x, y, z);
+      firstPersonViewCamera.lookAt(lookAtX, lookAtY, lookAtZ);
+    };
+    return { setCamera, firstPersonViewCamera };
+  })();
+
   const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
   directionalLight.position.set(5, 10, 7.5);
   scene.add(directionalLight);
 
   const controls = new OrbitControls(camera, renderer.domElement);
-  camera.position.set(0, 20, 100);
+  camera.position.set(0, 10, 10);
   controls.update();
 
   const gridHelper = new THREE.GridHelper(20, 20);
   scene.add(gridHelper);
 
   const loader = new GLTFLoader();
-  loader.load("/ap-simulator/public/demo.glb", (model) => {
+  loader.load(MODEL, (model) => {
     scene.add(model.scene);
   });
 
-  camera.position.z = 5;
+  const raycaster = new THREE.Raycaster();
+  const onFindPath = (event) => {
+    const mouse = new THREE.Vector2();
+    const rect = canvas.getBoundingClientRect();
+    mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+    mouse.y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
+
+    // Set the raycaster position based on the mouse coordinates
+    raycaster.setFromCamera(mouse, camera);
+
+    // Check for intersections
+    const intersects = raycaster.intersectObjects(scene.children, true);
+
+    if (intersects.length > 0) {
+      // Get the coordinates of the first intersection point
+      const { point } = intersects[0];
+      const path = pathFinder.getPathFromA2B(
+        firstPersonViewCamera.position,
+        point
+      );
+      if (!path) return null;
+      return [firstPersonViewCamera.position, ...path];
+    }
+    return null;
+  };
 
   const animate = () => {
     requestAnimationFrame(animate);
@@ -52,6 +98,8 @@ function ThreeApp(canvas) {
 
   return {
     resizeCanvas,
+    onFindPath,
+    setCamera,
   };
 }
 
--- src/ThreeCanvas.jsx
+++ src/ThreeCanvas.jsx
@@ -7,10 +7,26 @@ const FullScreenCanvas = () => {
     const canvas = canvasRef.current;
     const core = new ThreeApp(canvas);
 
+    core.setCamera([-3, 0.2, 5], [-3, 0.2, 4]);
+
+    const onPointerUp = (e) => {
+      const path = core.onFindPath(e);
+
+      if (path) {
+        console.log(path);
+        core.setCamera(
+          [path[0].x, path[0].y, path[0].z],
+          [path[1].x, path[1].y, path[1].z]
+        );
+      }
+    };
+
     window.addEventListener("resize", core.resizeCanvas);
+    canvas.addEventListener("pointerup", onPointerUp);
 
     return () => {
       window.removeEventListener("resize", core.resizeCanvas);
+      canvas.removeEventListener("pointerup", onPointerUp);
     };
   }, []);
 
