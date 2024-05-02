5c875f9d78b0656ed8c4cf110a7f9a12e000bb4c
yushiang <tsengyushiang@gmail.com>
2024-05-03 00:17:38 +0800

    fix: get wrong 3d point from click.

--- src/ThreeCanvas.jsx
+++ src/ThreeCanvas.jsx
@@ -1,36 +1,67 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ThreeApp from "./ThreeApp";
 const FullScreenCanvas = () => {
+  const [core, setCore] = useState(null);
+  const [isTeleport, setIsTeleport] = useState(true);
   const canvasRef = useRef(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    const core = new ThreeApp(canvas);
+    const threeApp = new ThreeApp(canvas);
+    setCore(threeApp);
+  }, []);
 
-    core.setCamera([-3, 1, 5], [-3, 1, 4]);
-
+  useEffect(() => {
+    if (!core) return;
+    const canvas = canvasRef.current;
     const onPointerUp = (e) => {
-      const path = core.onFindPath(e);
+      const point = core.getPointFromClick(e);
 
-      if (path) {
-        core.setPath(path);
+      if (isTeleport) {
+        if (point)
+          core.setCamera(
+            [point.x, point.y, point.z],
+            [point.x, point.y, point.z - 1]
+          );
+        return;
+      }
+
+      if (point) {
+        const path = core.findPathTo(point);
+
+        if (path) {
+          core.setCamera(
+            [path[0].x, path[0].y, path[0].z],
+            [path[1].x, path[1].y, path[1].z]
+          );
+          core.setPath(path);
+        }
       }
     };
 
     window.addEventListener("resize", core.resizeCanvas);
+    canvas.addEventListener("pointerdown", core.onSelectSensor);
     canvas.addEventListener("pointerup", onPointerUp);
 
     return () => {
       window.removeEventListener("resize", core.resizeCanvas);
+      canvas.addEventListener("pointerdown", core.onSelectSensor);
       canvas.removeEventListener("pointerup", onPointerUp);
     };
-  }, []);
+  }, [core, isTeleport]);
 
   return (
-    <canvas
-      ref={canvasRef}
-      style={{ width: "100%", height: "100%", display: "block" }}
-    />
+    <>
+      <div style={{ position: "absolute" }}>
+        <button onClick={() => setIsTeleport((prev) => !prev)}>
+          {isTeleport ? "click to teleport" : "click to find path"}
+        </button>
+      </div>
+      <canvas
+        ref={canvasRef}
+        style={{ width: "100%", height: "100%", display: "block" }}
+      />
+    </>
   );
 };
 
