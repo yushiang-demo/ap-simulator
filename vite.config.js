e851dbefd5375246ef1ee47880af05ede8a5073a
yushiang <tsengyushiang@gmail.com>
2024-05-05 15:33:17 +0800

    fix: vite reload in docker.

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/triangle-localization-simulator",
+  server: {
+    host: "0.0.0.0",
+    watch: {
+      usePolling: true,
+    },
+    hmr: { host: "0.0.0.0" },
+  },
 });
